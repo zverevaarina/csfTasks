4d79d011ff0a8f3d84b35b89ebd7b1c164a0b1f0
Арина <@->
2018-03-17 21:14:28 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/File_Ex_2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class File_Ex_2
+    {
+        public string[] Path { get; set; }
+        public byte[] FileText1 { get; set; }
+        public byte[] FileText2 { get; set; }
+
+        public File_Ex_2(string [] path)
+        {
+            this.Path = path;
+            this.FileText1 = IOUtils.FileToBytes(path[0]);
+            this.FileText2 = IOUtils.FileToBytes(path[1]);
+
+        }
+
+        public void Swap(/*byte[] FileText1, byte[] FileText2*/)
+        {
+            if (FileText1.Length > FileText2.Length)
+            {
+                byte[] tmp = new byte [FileText1.Length];
+                tmp = FileText1;
+                FileText1 = FileText2;
+                FileText2 = tmp;
+
+            }
+            else
+            {
+                byte[] tmp = new byte[FileText2.Length];
+                tmp = FileText2;
+                FileText2 = FileText1;
+                FileText1 = tmp;
+            }
+        }
+    }
+}
---
+++ ClassLibrary/Text_Ex_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Text_Ex_1
+    {
+        public string Path { get; set; }
+        public string [] Lines { get; set; }
+
+        public Text_Ex_1 (string path)
+        {
+            this.Path = path;
+            this.Lines = IOUtils.FileToArray(path);
+        }
+
+        public string[] NewLines(int k)
+        {
+            string[] num = new string[k];
+            int index = 0;
+            for (int i = Lines.Length - k; i < Lines.Length; i++)
+            {
+                num[index] = Lines[i];
+                index++;
+            }
+            return num;
+        }
+
+    }
+}
